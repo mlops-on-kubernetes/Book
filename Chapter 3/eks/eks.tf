0464101ed59450979d8d1bf7767480b673964a0f
Re Alvarez Parmar <re.alvrz@gmail.com>
2024-06-04 19:44:23 +0000

    add support for airflow

--- Chapter 3/eks/eks.tf
+++ Chapter 3/eks/eks.tf
@@ -31,9 +31,16 @@ module "eks" {
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
+    aws-ebs-csi-driver     = {
+      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn 
+    }
+    aws-efs-csi-driver     = {
+      service_account_role_arn = module.efs_csi_driver_irsa.iam_role_arn 
+    }
   }
 
   enable_efa_support = true
+  enable_irsa = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -45,6 +52,7 @@ module "eks" {
       min_size     = 2
       max_size     = 3
       desired_size = 2
+      disk_size    = 50
 
       labels = {
         # Used to ensure Karpenter runs on nodes that it does not manage
@@ -73,7 +81,144 @@ module "eks" {
 
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}"
+  value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.oidc_provider_arn}"
+}
+
+################################################################################
+# EBS Configuration
+################################################################################
+
+module "ebs_csi_driver_irsa" {
+ source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+ role_name             = "${local.name}-ebs-csi-driver"
+ role_policy_arns = {
+   policy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+ }
+ oidc_providers = {
+  main = {
+     provider_arn               = module.eks.oidc_provider_arn
+     namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+  }
+ }
+ tags = local.tags
+}
+
+
+################################################################################
+# EFS configuration
+################################################################################
+module "efs_csi_driver_irsa" {
+ source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+ role_name             = "${local.name}-efs-csi-driver"
+ role_policy_arns = {
+   policy = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+ }
+ oidc_providers = {
+  main = {
+     provider_arn               = module.eks.oidc_provider_arn
+     namespace_service_accounts = ["kube-system:efs-csi-controller-sa"]
+  }
+ }
+ tags = local.tags
+}
+
+resource "kubernetes_annotations" "efs-service-account-annotation" {
+  api_version = "v1"
+  kind        = "ServiceAccount"
+
+   metadata {
+     name      = "efs-csi-controller-sa"
+     namespace = "kube-system"
+   }
+
+  annotations = {
+   "eks.amazonaws.com/role-arn" = module.efs_csi_driver_irsa.iam_role_arn
+  }
+}
+
+resource "aws_efs_file_system" "efs-share" {
+  creation_token = "ml-share"
+
+  tags = {
+    Name = "ml-share"
+  }
+}
+
+resource "aws_efs_access_point" "efs-ap" {
+  file_system_id = aws_efs_file_system.efs-share.id
+}
+
+resource "aws_efs_mount_target" "efs-share" {
+  file_system_id  = aws_efs_file_system.efs-share.id
+  for_each        = toset(module.vpc.private_subnets)
+  subnet_id       = each.value
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_security_group" "efs" {
+  name        = "${local.name}-efs"
+  description = "Allow inbound NFS traffic from private subnets of the VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow NFS 2049/tcp"
+    cidr_blocks = [local.vpc_cidr]
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+  }
+
+  tags = local.tags
+}
+
+resource "kubernetes_storage_class" "efs" {
+  metadata {
+    name = "efs-sc"
+  }
+
+  storage_provisioner = "efs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  parameters = {
+   #fileSystemId     = aws_efs_file_system.efs-share.id
+   #directoryPerms   = "700"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "efs-pvc" {
+  metadata {
+    name = "efs-claim"
+    namespace = "jupyter"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "5Gi"
+      }
+    }
+    #volume_name = "${kubernetes_persistent_volume.example.metadata.0.name}"
+    storage_class_name = "efs-sc"
+  }
+}
+
+resource "kubernetes_persistent_volume" "jupter-efs-pv" {
+  metadata {
+    name = "efs-pv"
+  }
+  spec {
+    storage_class_name = "efs-sc"
+    capacity = {
+      storage = "5Gi"
+    }
+    access_modes = ["ReadWriteMany"]
+    persistent_volume_source {
+      csi {
+        driver        = "efs.csi.aws.com"
+        volume_handle = format("%s::%s", aws_efs_file_system.efs-share.id, aws_efs_access_point.efs-ap.id)
+        read_only     = false
+      }
+    }
+  }
 }
 
 ################################################################################
@@ -102,8 +247,8 @@ resource "helm_release" "karpenter" {
   namespace           = "kube-system"
   name                = "karpenter"
   repository          = "oci://public.ecr.aws/karpenter"
-  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  #repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
   version             = "0.36.2"
   wait                = false
