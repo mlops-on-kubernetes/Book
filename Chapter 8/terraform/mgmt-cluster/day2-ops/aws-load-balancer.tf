9256dfadafb12d42161ccf8041ac320741e30c4b
Elamaran Shanmugam <elamaran.shan@gmail.com>
2024-11-03 19:24:30 -0500

    Fixing ALB Issue

--- Chapter 8/terraform/mgmt-cluster/day2-ops/aws-load-balancer.tf
+++ Chapter 8/terraform/mgmt-cluster/day2-ops/aws-load-balancer.tf
@@ -19,8 +19,8 @@ resource "kubectl_manifest" "application_argocd_aws_load_balancer_controller" {
   depends_on = [ module.aws_load_balancer_role ]
   yaml_body = templatefile("${path.module}/templates/argocd-apps/aws-load-balancer.yaml", {
      CLUSTER_NAME = local.cluster_name
-     ROLE_ARN = module.aws_load_balancer_role.iam_role_arn
-     EKS_VPC_ID = data.aws_vpc.eks_vpc
+     MGMT_ROLE_ARN = module.aws_load_balancer_role.iam_role_arn
+     EKS_VPC_ID = data.aws_vpc.eks_vpc.id
      EKS_REGION = var.region
     }
   )
