f845e5e10f771d40afdef5811ffe41d4395b0615
Elamaran (Ela) Shanmugam <51791117+elamaran11@users.noreply.github.com>
2024-11-11 17:48:36 -0500

    Update uninstall.sh

--- Chapter 8/setups/uninstall.sh
+++ Chapter 8/setups/uninstall.sh
@@ -13,13 +13,6 @@ echo "Kubernetes cluster: $(kubectl config current-context)"
 echo "AWS profile (if set): ${AWS_PROFILE}"
 echo "AWS account number: $(aws sts get-caller-identity --query "Account" --output text)"
 
-echo -e "${RED}\nAre you sure you want to continue?${NC}"
-read -p '(yes/no): ' response
-if [[ ! "$response" =~ ^[Yy][Ee][Ss]$ ]]; then
-  echo 'exiting.'
-  exit 0
-fi
-
 cd "${TF_DIR}"
 terraform destroy
 
