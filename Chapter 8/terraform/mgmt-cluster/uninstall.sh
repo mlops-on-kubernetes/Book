8cbdb1ac52b71d669d3c5a19efffbc9e019e3338
Elamaran (Ela) Shanmugam <51791117+elamaran11@users.noreply.github.com>
2024-11-11 21:44:23 -0500

    Update uninstall.sh

--- Chapter 8/terraform/mgmt-cluster/uninstall.sh
+++ Chapter 8/terraform/mgmt-cluster/uninstall.sh
@@ -9,18 +9,7 @@ echo "Kubernetes cluster: $(kubectl config current-context)"
 echo "AWS profile (if set): ${AWS_PROFILE}"
 echo "AWS account number: $(aws sts get-caller-identity --query "Account" --output text)"
 
-echo -e "${RED}\nAre you sure you want to continue?${NC}"
-read -p '(yes/no): ' response
-if [[ ! "$response" =~ ^[Yy][Ee][Ss]$ ]]; then
-  echo 'exiting.'
-  exit 0
-fi
-
 SETUP_DIR="${REPO_ROOT}/${SUB_LEVEL}/setups"
-
-cd "${SETUP_DIR}/argocd/"
-./uninstall.sh
-cd -
 
 cd "${REPO_ROOT}/${SUB_LEVEL}/terraform/mgmt-cluster"
 
