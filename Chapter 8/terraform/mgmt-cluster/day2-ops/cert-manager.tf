40b1b3d772f573d6693fc750b69901122272c3b5
Elamaran (Ela) Shanmugam <51791117+elamaran11@users.noreply.github.com>
2024-11-11 21:26:52 -0500

    Update cert-manager.tf

--- Chapter 8/terraform/mgmt-cluster/day2-ops/cert-manager.tf
+++ Chapter 8/terraform/mgmt-cluster/day2-ops/cert-manager.tf
@@ -2,12 +2,8 @@ resource "kubectl_manifest" "application_argocd_cert_manager" {
   yaml_body = templatefile("${path.module}/templates/argocd-apps/cert-manager.yaml", {
     REPO_URL = local.repo_url
   })
-}
-
-resource "terraform_data" "wait_for_cert_manager" {
   provisioner "local-exec" {
-    command = "kubectl wait --for=jsonpath=.status.health.status=Healthy -n argocd application/cert-manager && kubectl wait --for=jsonpath=.status.sync.status=Synced --timeout=900s -n argocd application/cert-manager"
+    command = "kubectl wait --for=jsonpath=.status.health.status=Healthy --timeout=300s -n argocd application/cert-manager"
+    interpreter = ["/bin/bash", "-c"]
   }
-
-  depends_on = [kubectl_manifest.application_argocd_cert_manager]
 }
